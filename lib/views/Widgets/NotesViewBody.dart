31bb66c38c28181807fc560485eef9bb8df72258
ahmed-abmouty <ahmed122727727@gmail.com>
2023-07-10 17:47:58 +0300

    add note bottom sheet

--- lib/views/Widgets/NotesViewBody.dart
+++ lib/views/Widgets/NotesViewBody.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app/views/Widgets/AddNoteBottomSheet.dart';
 import 'package:note_app/views/Widgets/CustomAppBar.dart';
 import 'package:note_app/views/Widgets/CustomNoteITem.dart';
 
@@ -9,23 +10,32 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        
-      },child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNoteBottomSheet();
+              });
+        },
+        child: Icon(Icons.add),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: Column(
             children: [
               CustomAppBar(),
-              SizedBox(height: 40,),
+              SizedBox(
+                height: 40,
+              ),
               Expanded(
                 child: ListView.separated(
                     itemBuilder: (context, index) => CustomNoteITem(
-                      noteDate: DateTime.now().toString().substring(0,11),
-                      noteText: 'text',
-                      noteTitle: 'thitle',
-                    ),
+                          noteDate: DateTime.now().toString().substring(0, 11),
+                          noteText: 'text',
+                          noteTitle: 'thitle',
+                        ),
                     separatorBuilder: (context, index) => SizedBox(height: 20),
                     itemCount: 10),
               ),
